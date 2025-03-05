8e16c4664b9e7347c9949ab76c9ec574c0d673c5
KISHORE RAM M <mrkisho65@outlook.com>
2025-03-05 16:26:23 +0530

    Add files via upload
    basic config-1

---
+++ Microservices/Department-Service/Department-Service/src/test/java/Microsevices/Department_Service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Microsevices.Department_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
